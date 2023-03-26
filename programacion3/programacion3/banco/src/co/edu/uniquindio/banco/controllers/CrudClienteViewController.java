a51e3222ba0bb054eb37427e366b0800d31cc3f5
Bryan1625 <bryangomez1625@gmail.com>
2023-03-26 18:27:54 -0500

    nueva estructura y archivos

---
+++ programacion3/programacion3/banco/src/co/edu/uniquindio/banco/controllers/CrudClienteViewController.java
@@ -0,0 +1,15 @@
+package co.edu.uniquindio.banco.controllers;
+
+import co.edu.uniquindio.banco.model.Banco;
+
+public class CrudClienteViewController {
+
+	ModelFactoryController modelFactoryController;
+	
+	public CrudClienteViewController(ModelFactoryController modelFactoryController) {
+		this.modelFactoryController = modelFactoryController;
+	}
+
+
+
+}
--- programacion3/programacion3/co.edu.uniquindio.banco/src/main/java/co/edu/uniquindio/banco/model/Estado.java
+++
@@ -1,6 +0,0 @@
-package co.edu.uniquindio.banco.model;
-
-public enum Estado {
-
-    Exitosa, Rechazada, SinFondos
-}
